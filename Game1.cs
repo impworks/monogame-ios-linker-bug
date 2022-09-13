804b32cdf00a20afe1b1601c9d25ab1d081541da
Andrei Kurosh <mail@spark.ms>
2022-09-13 17:17:04 +0300

    Reproduction code.

---
+++ Game1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace iPadTest
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _graphics.GraphicsDevice.Clear(Color.Yellow);
+            base.Draw(gameTime);
+        }
+    }
+}
